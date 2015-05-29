1c99d323246e7b0d4b6067999945065c41b5f38a
David Lawrence <David.Lawrence@unige.ch>
2015-05-29 14:00:56 +0200

    Added comments

--- tests/dynarray_tests.c
+++ tests/dynarray_tests.c
@@ -11,11 +11,12 @@ void create_array_test1(void **state) {
 /* A testcase that test the creation of an array with a given
  maximum bound */
 void create_array_test2(void **state) {
-	dyn_array * array = new_array_wbounds(2);
+	signed int bound = 2;
+	dyn_array * array = new_array_wbounds(bound);
+
 	assert_non_null(array);
 	assert_true(array->nb_elems == 0);
 	assert_true(array->max_size == 2);
-
 }
 
 /* A testcase that test the creation of an array with a
@@ -25,7 +26,8 @@ void create_array_test3(void **state) {
 	assert_null(array);
 }
 
-
+/* A testcase that test the addition of element of the end of an array with a
+ maximum bound of 2 */
 void append_test1(void **state) {
 	dyn_array * array = new_array_wbounds(2);
 	assert_non_null(array);
@@ -43,6 +45,8 @@ void append_test1(void **state) {
 	assert_true(array->max_size == 2);
 }
 
+/* A testcase that test the addition of 2++ elements of the end of an array with a
+ maximum bound of 2. The array should resize */
 void append_test2(void **state) {
 	dyn_array * array = new_array_wbounds(2);
 	assert_non_null(array);
@@ -52,33 +56,34 @@ void append_test2(void **state) {
 	append_element(array, 2);
 	assert_true(array->max_size == 2);
 	
+	// Add one more element than the maximal capacity
 	append_element(array, 3);
 	assert_true(array->max_size == 4);
 	assert_true(array->elems[2] == 3);
-		
+	
+	// Add two more element to resize again the array
 	append_element(array, 4);
 	append_element(array, 5);
-	
 	assert_true(array->max_size == 6);
 }
 
+/* A testcase that test the addition of 50++ elements of the end of an array with a
+ maximum bound of 50. The array should resize */
 void append_test3(void **state) {
 	dyn_array * array = new_array();
 	assert_non_null(array);
 	assert_true(array->max_size == MAX_ELEMS);
 	
 	for (int i=0; i<MAX_ELEMS; i++) append_element(array, i);
-	
 	assert_true(array->nb_elems == MAX_ELEMS);
 	
 	append_element(array, 3);
-
 	assert_true(array->max_size == MAX_ELEMS * 2);
 	assert_true(array->nb_elems == MAX_ELEMS + 1);
 	
 }
 
-
+/* A testcase that test the normal setting and getting of elements of the array */
 void getset_test1(void ** state) {
 	dyn_array * array = new_array();
 	assert_non_null(array);
@@ -93,7 +98,7 @@ void getset_test1(void ** state) {
 	for (int i=0; i<MAX_ELEMS; i++) assert_true(10 == array->elems[i]);
 }
 
-
+/* A testcase that test the getting of elements out of the bounds of the array */
 void get_test1(void ** state) {
 	dyn_array * array = new_array();
 	assert_non_null(array);
@@ -104,7 +109,7 @@ void get_test1(void ** state) {
 	assert_true(get_element(array, 7, 10) == -1);
 }
 
-
+/* A testcase that test the setting of elements out of the bounds of the array */
 void set_test1(void ** state) {
 	dyn_array * array = new_array();
 	assert_non_null(array);
--- tests/dynarray_tests.h
+++ tests/dynarray_tests.h
@@ -7,14 +7,17 @@
 #include <cmocka.h>
 #include "dynarray.h"
 
+// Testing the creation of a dynamic array
 void create_array_test1(void **state);
 void create_array_test2(void **state);
 void create_array_test3(void **state);
 
+// Testing the addition of elements at the end of an array
 void append_test1(void **state);
 void append_test2(void **state);
 void append_test3(void **state);
 
+// Testing the get and set of an array
 void getset_test1(void ** state);
 void get_test1(void ** state);
 void set_test1(void ** state);

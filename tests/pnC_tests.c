cdfb2a07b4d797d285132e83ff3457762cda5443
David Lawrence <David.Lawrence@unige.ch>
2015-05-29 10:39:45 +0200

    Added more tests

--- tests/pnC_tests.c
+++ tests/pnC_tests.c
@@ -2,7 +2,7 @@
 
 /* A test case that test the normal creation of a PN */
 void create_pn_test1(void **state) {
-	int marking[1] = {1};
+	signed int marking[1] = {1};
 	struct PN * pn = new_pn(1, 1, marking);
 	
 	assert_non_null(pn);
@@ -22,10 +22,14 @@ void create_pn_test2(void **state) {
 }
 
 /* A test case that test the abnormal creation of a PN */
-void create_pn_test3(void **state) {
-	int marking[1] = {1};
-	struct PN * pn = new_pn(0, 0, marking);
-	
-	assert_null(pn);
-	//(void) state; /* unused */
+void add_pre_arc_test1(void **state) {
+	int marking[1] = {1,0};
+	struct PN * pn = new_pn(2, 1, marking);
+	assert_non_null(pn);
+
+	assert_return_code(add_pre_arc(pn, 0, 0, 1), 0);
+	assert_return_code(add_pre_arc(pn, 1, 0, 1), 0);
+
+	destroy_pn(pn);
+
 }
--- tests/pnC_tests.h
+++ tests/pnC_tests.h
@@ -10,5 +10,6 @@
 void create_pn_test1(void **state);
 void create_pn_test2(void **state);
 void create_pn_test3(void **state);
+void add_pre_arc_test1(void **state);
 
 #endif

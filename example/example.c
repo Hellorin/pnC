1fd993457c3c5b3c56452887354056503f1e459a
David Lawrence <David.Lawrence@unige.ch>
2016-01-14 13:42:26 +0100

    Improved example

--- example/example.c
+++ example/example.c
@@ -1,11 +1,29 @@
 #include "pnC.h"
 
 int main(void) {
-	int marking[2] = {1,0}; 
-	struct PN * pn = new_pn(2, 1, marking);
-	add_pre_arc(pn, 0, 0, 1);
-	add_post_arc(pn, 1, 0, 1);
-	
-	int * marking2 = fire_transition(pn, 0);
-	for (int i=0; i<2; i++) printf("%i ",marking2[i]);
+    // Create the initial marking
+    int marking[2] = {1,0};
+
+    // Create the Petri net with the initial marking,
+    // 2 places and 1 transition
+    struct PN * pn = new_pn(2, 2, marking);
+
+    // Add a pre arc to the transition 0 of the Petri net pn of weight 1
+    add_pre_arc(pn, 0, 0, 1);
+    // Add a post arc to the transition 0 of the Petri net pn of weight 1
+    add_post_arc(pn, 1, 0, 1);
+
+    printf("Marking before firing the transition t0\n");
+    for (int i=0; i<2; i++) printf("p%i: %i\n", i, get_marking(pn)[i]);
+    printf("\n");
+
+    // Fire the transition 0
+    int * marking2 = fire_transition(pn, 0);
+    set_marking(pn, marking2, 2);
+    printf("\n");
+
+    printf("Marking after firing the transition t0\n");
+    for (int i=0; i<2; i++) printf("p%i: %i\n", i, get_marking(pn)[i]);
+
+    printf("\n");
 }
